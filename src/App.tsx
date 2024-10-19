5517830296efea4c14b8362330cb23862f00484a
ericgalbarn <ericgalbarn@gmail.com>
2024-10-20 00:35:30 +0700

    Initialize the Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CompanyCard from "./Components/Card/CompanyCard";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CompanyCard></CompanyCard>
+    </>
+  );
+}
+
+export default App;
