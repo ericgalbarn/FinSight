e1894d9e5d361022f4e7e6f755ec2cbec84ee579
ericgalbarn <ericgalbarn@gmail.com>
2024-10-24 16:54:17 +0700

    Initializing Financial Modeling Prep API

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { searchCompanies } from "./api.tsx";
 
+console.log(searchCompanies("AA"));
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
