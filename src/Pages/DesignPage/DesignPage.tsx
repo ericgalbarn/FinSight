9eaab1c92c79bf6ad235c81914ccca51d69d3453
ericgalbarn <ericgalbarn@gmail.com>
2024-11-05 18:06:48 +0700

    Making a Ratio List for the Company in the Design Guide

--- src/Pages/DesignPage/DesignPage.tsx
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Table from "../../Components/Table/Table";
+import RatioList from "../../Components/RatioList/RatioList";
 
 interface Props {}
 
@@ -7,10 +8,7 @@ const DesignPage = (props: Props) => {
   return (
     <>
       <h1>FinSight Design Page</h1>
-      <h2>
-        This is FinSight's design page. This is where we will house various
-        design elements of the app
-      </h2>
+      <RatioList />
       <Table />
     </>
   );
